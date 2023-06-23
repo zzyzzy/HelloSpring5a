7eeba28bc947b317e53d046e485de1b9fad56dc9
JaeSeong Song <zzyzigy@naver.com>
2023-06-23 11:19:45 +0900

    sayHello 메서드 분리

--- src/main/java/hello/spring/basic/Spring5App00.java
+++ src/main/java/hello/spring/basic/Spring5App00.java
@@ -5,6 +5,13 @@ public class Spring5App00 {
     // 간단한 인삿말을 출력하는 프로그램 작성
 
     public static void main(String[] args) {
+        // sayHello 메서드를 호출하기 위해
+        // Spring5App00의 객체화 필요!!
+        Spring5App00 app00 = new Spring5App00();
+        app00.sayHello();
+    }
+
+    private void sayHello() {
         System.out.println("Hello, World!!");
     }
 
