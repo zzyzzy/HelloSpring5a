9ae6587ab9b1cefa2f8f1c43e2c293bdc42b1899
JaeSeong Song <zzyzigy@naver.com>
2023-06-23 11:04:33 +0900

    spring 첫번째 사례

---
+++ src/main/java/hello/spring/basic/Spring5App00.java
@@ -0,0 +1,11 @@
+package hello.spring.basic;
+
+public class Spring5App00 {
+    // 첫번째 사례
+    // 간단한 인삿말을 출력하는 프로그램 작성
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!!");
+    }
+
+}
